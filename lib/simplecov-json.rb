1d54d90eeeca5d8141b135c2023eb6e22a1b4769
pi-chan <hiromasa.ohno.1007@gmail.com>
2020-12-25 07:26:18 +0900

    add support for group

--- lib/simplecov-json.rb
+++ lib/simplecov-json.rb
@@ -2,7 +2,6 @@ require 'simplecov'
 require 'json'
 
 class SimpleCov::Formatter::JSONFormatter
-  
   def format(result)
     data = {}
     data[:timestamp] = result.created_at.to_i
@@ -18,9 +17,9 @@ class SimpleCov::Formatter::JSONFormatter
         filename: sourceFile.filename,
         covered_percent: sourceFile.covered_percent,
         coverage: coverage_data,
-        covered_strength: sourceFile.covered_strength.nan? ? 0.0 : sourceFile.covered_strength, 
-        covered_lines: sourceFile.covered_lines.count, 
-        lines_of_code: sourceFile.lines_of_code, 
+        covered_strength: sourceFile.covered_strength.nan? ? 0.0 : sourceFile.covered_strength,
+        covered_lines: sourceFile.covered_lines.count,
+        lines_of_code: sourceFile.lines_of_code,
       }
     end
     data[:metrics] = {
@@ -29,34 +28,43 @@ class SimpleCov::Formatter::JSONFormatter
       covered_lines: result.covered_lines,
       total_lines: result.total_lines
     }
-    
+    data[:groups] = []
+    result.groups.each do |group_name, group|
+      data[:groups] << {
+        group_name: group_name,
+        covered_percent: group.covered_percent,
+        covered_strength: group.covered_strength.nan? ? 0.0 : group.covered_strength,
+        covered_lines: group.covered_lines,
+        lines_of_code: group.lines_of_code,
+      }
+    end
+
     json = data.to_json
-    
+
     File.open(output_filepath, "w+") do |file|
       file.puts json
     end
-    
+
     puts output_message(result)
-    
+
     json
   end
-  
+
   def output_filename
     'coverage.json'
   end
-  
+
   def output_filepath
     File.join(output_path, output_filename)
   end
-  
+
   def output_message(result)
     "Coverage report generated for #{result.command_name} to #{output_filepath}. #{result.covered_lines} / #{result.total_lines} LOC (#{result.covered_percent.round(2)}%) covered."
   end
-  
-private
-  
+
+  private
+
   def output_path
     SimpleCov.coverage_path
   end
-  
 end
